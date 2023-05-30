fdf7253ef8a10423a1e590ca4808a71dcfcb221f
Logan Welch <loganewelch@gmail.com>
2023-05-30 14:21:23 -0500

    up to part eight completed

---
+++ src/components/tickets/TicketList.js
@@ -0,0 +1,101 @@
+import { useEffect, useState } from "react"
+import { useNavigate } from "react-router-dom"
+import "./Tickets.css"
+
+export const TicketList = () => {
+    const [tickets, setTickets] = useState([])
+    const [filteredTickets, setFiltered] = useState([])
+    const [emergency, setEmergency] = useState(false)
+    const [openOnly, updateOpenOnly] = useState(false)
+    const navigate = useNavigate()
+
+    const localHoneyUser = localStorage.getItem("honey_user")
+    const honeyUserOBject = JSON.parse(localHoneyUser)
+
+
+    useEffect(
+        () => {
+            if (emergency) {
+                const emergencyTickets = tickets.filter(ticket => ticket.emergency === true)
+                setFiltered(emergencyTickets)
+            }
+            else {
+                setFiltered(tickets)
+            }
+        },
+        [emergency]
+    )
+
+    useEffect(
+        () => {
+            fetch(`http://localhost:8088/serviceTickets`)
+                .then(response => response.json())
+                .then((ticketArray) => {
+                    setTickets(ticketArray)
+
+                })
+            //console.log("Initial state of tickets", tickets) // View the initial state of tickets
+        },
+        [] // When this array is empty, you are observing initial component state
+    )
+
+    useEffect(
+        () => {
+            if (honeyUserOBject.staff) {
+                // for employees
+                setFiltered(tickets)
+            }
+            else {
+                // for customers
+                const myTickets = tickets.filter(ticket => ticket.userId === honeyUserOBject.id)
+                setFiltered(myTickets)
+            }
+        },
+        [tickets]
+    )
+
+    useEffect(
+        () => {
+            if (openOnly) {
+                const openTicketArray = tickets.filter(ticket => {
+                    return ticket.userId === honeyUserOBject.id && ticket.dateCompleted === ""
+                })
+                setFiltered(openTicketArray)
+            }
+            else {
+                const myTickets = tickets.filter(ticket => ticket.userId === honeyUserOBject.id)
+                setFiltered(myTickets)
+            }
+        },
+        [openOnly]
+    )
+
+    return <>
+        {
+            honeyUserOBject.staff
+                ? <>
+                    <button onClick={() => { setEmergency(true) }} >Emergency Only</button>
+                    <button onClick={() => { setEmergency(false) }} >Show All</button>
+                </>
+                : <>
+                    <button onClick={() => navigate("/ticket/create")}>Create Ticket</button>
+                    <button onClick={() => updateOpenOnly(true)}>Open Tickets</button>
+                    <button onClick={() => updateOpenOnly(false)}>All My Tickets</button>
+                </>
+        }
+        <h2>List of Tickets</h2>
+
+        <article className="tickets">
+            {
+                filteredTickets.map(
+                    (ticket) => {
+                        return <section className="ticket" key={(`ticket--${ticket.id}`)}>
+                            <header>{ticket.description}</header>
+                            <footer>Emergency: {ticket.emergency ? "🍌" : "No"}</footer>
+                        </section>
+                    }
+                )
+            }
+        </article>
+    </>
+}
